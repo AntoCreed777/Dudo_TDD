a9f48e046b40d5a6734b59ef979e2d7d50a5908e
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-21 17:17:41 -0400

    GREEN: Correccion de test y implementacion de traduccion de numeros a nombres en Dado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -23,8 +23,8 @@ class TestDado:
     
     def test_numero_a_nombre(self, dado):
         assert dado.numero_a_nombre(1) == "As"
-        assert dado.numero_a_nombre(2) == "Dos"
-        assert dado.numero_a_nombre(3) == "Tres"
-        assert dado.numero_a_nombre(4) == "Cuatro"
-        assert dado.numero_a_nombre(5) == "Cinco"
-        assert dado.numero_a_nombre(6) == "Seis"
+        assert dado.numero_a_nombre(2) == "Tonto"
+        assert dado.numero_a_nombre(3) == "Tren"
+        assert dado.numero_a_nombre(4) == "Cuadra"
+        assert dado.numero_a_nombre(5) == "Quina"
+        assert dado.numero_a_nombre(6) == "Sexto"
