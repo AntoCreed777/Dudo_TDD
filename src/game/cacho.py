0343941cdae76fae4b4e1271ddc611b04375f650
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 13:06:13 -0400

    GREEN: Implementacion de cacho.agitar()
    Se crean 5 dados en un inicio con tal de reutilizarlos

--- src/game/cacho.py
+++ src/game/cacho.py
@@ -1,2 +1,17 @@
+from src.game.dado import Dado
+
+
 class Cacho:
-    pass
+    _dados: list[Dado]
+
+    def __init__(self):
+        self._dados = []
+        for i in range(5):  # Se crean la cantidad maxima de dados que pueden haber dentro del cacho
+            self._dados.append(Dado())
+
+    def agitar(self, cantidad: int) -> list[str]:
+        resultados = []
+        for i in range(cantidad):
+            self._dados[i].generar_numero()
+            resultados.append(self._dados[i].get_valor())
+        return resultados
