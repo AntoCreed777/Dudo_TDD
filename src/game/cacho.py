ec279fdd35548c4754d2dc72ebe2215b663bcdd9
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 13:38:58 -0400

    GREEN: implementacion d ecacho.get_resultados()

--- src/game/cacho.py
+++ src/game/cacho.py
@@ -3,8 +3,10 @@ from src.game.dado import Dado
 
 class Cacho:
     _dados: list[Dado]
+    _cantidad_agitada: int
 
     def __init__(self):
+        self._cantidad_agitada = 0
         self._dados = []
         for i in range(5):  # Se crean la cantidad maxima de dados que pueden haber dentro del cacho
             self._dados.append(Dado())
@@ -16,7 +18,15 @@ class Cacho:
         if cantidad > 5:
             cantidad = 5
 
+        self._cantidad_agitada = cantidad
+
         resultados = []
         for i in range(cantidad):
             self._dados[i].generar_numero()
             resultados.append(self._dados[i].get_valor())
+
+    def get_resultados(self) -> list[str]:
+        resultados = []
+        for i in range(self._cantidad_agitada):
+            resultados.append(self._dados[i].get_valor())
+        return resultados
