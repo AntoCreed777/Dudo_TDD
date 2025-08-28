f84e12427280df10156d1aebe00cbd32209819c2
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 13:45:51 -0400

    GREEN: Implementacion de cacho.ocultar()

--- src/game/cacho.py
+++ src/game/cacho.py
@@ -4,8 +4,10 @@ from src.game.dado import Dado
 class Cacho:
     _dados: list[Dado]
     _cantidad_agitada: int
+    _oculto: bool
 
     def __init__(self):
+        self._oculto = False
         self._cantidad_agitada = 0
         self._dados = []
         for i in range(5):  # Se crean la cantidad maxima de dados que pueden haber dentro del cacho
@@ -25,8 +27,14 @@ class Cacho:
             self._dados[i].generar_numero()
             resultados.append(self._dados[i].get_valor())
 
-    def get_resultados(self) -> list[str]:
+    def get_resultados(self) -> list[str] | None:
+        if self._oculto:
+            return None
+
         resultados = []
         for i in range(self._cantidad_agitada):
             resultados.append(self._dados[i].get_valor())
         return resultados
+
+    def ocultar(self):
+        self._oculto = True
