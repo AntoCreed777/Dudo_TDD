344ba4c17832dc104bf3d42b3b3b566a66a48d10
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 16:47:53 -0400

    GREEN: Implementacion de jugador.ver_cacho()

--- src/game/jugador.py
+++ src/game/jugador.py
@@ -12,3 +12,14 @@ class Jugador:
     def agitar_cacho(self):
         self._cacho.agitar(cantidad=self._dados_en_posecion)
         print("Cacho Agitado")
+
+    def ver_cacho(self):
+        print("Tu cacho:")
+
+        resultados = self._cacho.get_resultados()
+        if resultados is None:
+            print("\tNo tienes dados para ver, tu cacho esta vacio")
+            return
+
+        for i, resultado in enumerate(resultados):
+            print(f"\tDado {i + 1}: {resultado}")
