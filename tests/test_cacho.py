7b200392c0fdea0898bba89a4a27285794bbee6d
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 13:30:53 -0400

    RED: Se modifica test con tal de que valide estado de objetos internos
    Se debe de eliminar el return del metodo cacho.agitar()

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -12,3 +12,29 @@ class TestCacho:
         for cantidad in range(6):   # Pruebo desde 0 hasta 5 dados
             resultados = cacho.agitar(cantidad=cantidad)
             assert len(resultados) == cantidad
+
+    def test_agitar_cantidad_superior_a_5(self, cacho):
+        cantidad_ingresada = 6
+        cantidad_esperada = 5
+
+        cacho.agitar(cantidad=cantidad_ingresada)
+        contador = 0
+
+        for dado in cacho._dados:
+            if dado._valor is not None:
+                contador += 1
+        assert cantidad_esperada == contador, f"Deben de haber{cantidad_esperada} dados con valores asignados"
+
+    def test_agitar_cantidad_invalida(self, cacho):
+        with pytest.raises(ValueError, match="Cantidad a agitar invalida"):
+            cacho.agitar(cantidad=-1)
+
+    def test_get_resultados(self, cacho):
+        cantidad = 5
+
+        cacho.agitar(cantidad=cantidad)
+        resultados = cacho.get_resultados()
+
+        assert len(resultados) == cantidad
+        for resultado in resultados:
+            assert isinstance(resultado, str)
