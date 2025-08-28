d46a6275721f3cbf0f9bd8033df408083f78d4e6
AntoCreed777 <antbenavides2023@udec.cl>
2025-08-28 16:47:33 -0400

    RED: Test para jugador.ver_cacho()
    Debe de devolver una impresion por terminal en que se le indique al usuario los valores de los dados del cacho

--- tests/test_jugador.py
+++ tests/test_jugador.py
@@ -1,5 +1,6 @@
 import pytest
 from src.game.jugador import Jugador
+from src.game.dado import NombreDado
 
 
 class TestJugador:
@@ -14,3 +15,28 @@ class TestJugador:
         captura = capsys.readouterr()
 
         assert captura.out == "Cacho Agitado\n"
+
+    @pytest.mark.parametrize("parametros_input, impresion_esperada", [
+            (
+                [str(NombreDado.AS)],
+                "Cacho Agitado\nTu cacho:\n\tDado 1: As\n"
+            ),
+            (
+                [str(NombreDado.QUINA), str(NombreDado.TONTO)],
+                "Cacho Agitado\nTu cacho:\n\tDado 1: Quina\n\tDado 2: Tonto\n"
+            ),
+            (
+                None,
+                "Cacho Agitado\nTu cacho:\n\tNo tienes dados para ver, tu cacho esta vacio\n"
+            )
+        ]
+    )
+    def test_ver_cacho(self, mocker, capsys, jugador, parametros_input, impresion_esperada):
+        mocker.patch.object(jugador._cacho, "get_resultados", return_value=parametros_input)
+
+        jugador.agitar_cacho()
+        jugador.ver_cacho()
+
+        captura = capsys.readouterr()
+
+        assert captura.out == impresion_esperada
