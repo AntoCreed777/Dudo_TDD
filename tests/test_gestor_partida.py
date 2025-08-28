8be358edd7ce8165f0531fd7c63862e38a99ce4e
Ricardo Charris <rica14062004@gmail.com>
2025-08-28 19:13:44 -0400

    RED: test para definir al jugador que comienza el juego

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -1,21 +1,21 @@
 import pytest
 from src.game.gestor_partida import GestorPartida
 
-@pytest.fixture
-def cuatro_jugadores():
-    return 4
+@pytest.fixture(scope="class")
+def gestor_4_jugadores():
+    return GestorPartida(4)
 
 class TestGestorPartida:
     
 
-    def test_crear_jugadores(self, cuatro_jugadores):
-        gestor = GestorPartida(cuatro_jugadores)
-        assert len(gestor.jugadores) == 4
+    def test_crear_jugadores(self, gestor_4_jugadores):
+        assert len(gestor_4_jugadores.jugadores) == 4
 
 
-    @pytest.mark.skip(reason="Test aun no implementado")
-    def test_asignar_turnos(self):
-        pass
+    def test_definir_primer_jugador(self, mocker, gestor_4_jugadores):
+        mocker.patch("src.game.dado.random.randint", side_effect=[2, 2, 5, 2])
+        gestor_4_jugadores.definir_primer_jugador()
+        assert gestor_4_jugadores.turno_actual == gestor_4_jugadores.gestor_4_jugadores[2]
     
     @pytest.mark.skip(reason="Test aun no implementado")
     def test_jugar_ronda(self):
