c0ef612bafe744e7123bb2a01fd6f0311335e11e
martin777pro <martinfuentebiz22@gmail.com>
2025-09-01 03:26:49 -0400

    RED: valida las condiciones para poder calzar

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -50,3 +50,10 @@ class TestValidadorApuesta:
         assert not validador.puede_partir_con_ases(dados_en_mano=1, ya_usado=True)
         assert not validador.puede_partir_con_ases(dados_en_mano=2, ya_usado=False)
         assert validador.puede_partir_con_ases(dados_en_mano=1, ya_usado=False)
+
+    def test_puede_calzar(self, validador):
+        """Se puede calzar si hay la mitad o más de los dados máximos en juego, o el jugador que calza tiene 1 dado."""
+        assert validador.puede_calzar(dados_en_juego=12, dados_maximos=20, dados_del_jugador=3)
+        assert validador.puede_calzar(dados_en_juego=10, dados_maximos=20, dados_del_jugador=5)
+        assert not validador.puede_calzar(dados_en_juego=9, dados_maximos=20, dados_del_jugador=2)
+        assert validador.puede_calzar(dados_en_juego=9, dados_maximos=20, dados_del_jugador=1)
