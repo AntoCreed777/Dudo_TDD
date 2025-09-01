43350b78e351496177565ee9e3f15a9938169a51
Ricardo Charris <rica14062004@gmail.com>
2025-09-01 15:57:39 -0400

    REFACTOR: clase Apuesta guarda como atributo el tipo de apuesta

--- src/game/validador_apuesta.py
+++ src/game/validador_apuesta.py
@@ -1,6 +1,7 @@
 """Módulo que contiene las clases Apuesta y ValidadorApuesta para el juego Dudo."""
 
 from src.game.dado import NombreDado
+from src.game.jugador import TipoApuesta
 
 
 class Apuesta:
@@ -9,8 +10,9 @@ class Apuesta:
     # Luego hay que eliminar estos comentarios
     # pylint: disable=too-few-public-methods
 
-    def __init__(self, cantidad: int, pinta: NombreDado):
+    def __init__(self, tipo: str, cantidad: int, pinta: NombreDado):
         """Inicializa una apuesta con cantidad y pinta."""
+        self.tipo = tipo
         self.cantidad = cantidad
         self.pinta = pinta
 
@@ -24,19 +26,10 @@ class ValidadorApuesta:
 
     def es_valida(self, actual: Apuesta, nueva: Apuesta) -> bool:
         """Valida si una nueva apuesta es válida respecto a la actual."""
-        if actual.pinta != NombreDado.AS and nueva.pinta == NombreDado.AS:
-            return self._validar_cambio_a_ases(actual.cantidad, nueva.cantidad)
+        if actual.tipo == str(TipoApuesta.SUBIR).lower():
+            return self.puede_subir(actual, nueva)
 
-        if actual.pinta == NombreDado.AS and nueva.pinta != NombreDado.AS:
-            return self._validar_desde_ases(actual.cantidad, nueva.cantidad)
-
-        if nueva.pinta.value < actual.pinta.value:
-            return False
-
-        if nueva.pinta == actual.pinta:
-            return nueva.cantidad > actual.cantidad
-
-        return nueva.cantidad >= actual.cantidad
+        return False
 
     def _validar_cambio_a_ases(self, cantidad_actual: int, cantidad_nueva: int) -> bool:
         """Valida el cambio de una apuesta normal a Ases."""
@@ -55,3 +48,18 @@ class ValidadorApuesta:
         """Indica si se puede calzar con los dados actuales y del jugador."""
         mitad = (dados_maximos + 1) // 2
         return dados_del_jugador == 1 or dados_en_juego >= mitad
+
+    def puede_subir(self, actual: Apuesta, nueva: Apuesta):
+        if actual.pinta != NombreDado.AS and nueva.pinta == NombreDado.AS:
+            return self._validar_cambio_a_ases(actual.cantidad, nueva.cantidad)
+
+        if actual.pinta == NombreDado.AS and nueva.pinta != NombreDado.AS:
+            return self._validar_desde_ases(actual.cantidad, nueva.cantidad)
+
+        if nueva.cantidad > actual.cantidad:
+            return True
+
+        if nueva.cantidad < actual.cantidad:
+            return False
+
+        return nueva.pinta.value > actual.pinta.value
